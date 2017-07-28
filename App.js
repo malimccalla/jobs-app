6de5424b42861bdb932458e46a667d8578eae5a4
Mali Michael <malimccalla@gmail.com>
2017-07-28 23:03:55 +0100

    Full bones of navigation complete

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { StyleSheet, Text, View } from 'react-native';
+
+import AuthScreen from './screens/Auth.screen';
+import WelcomeScreen from './screens/Welcome.screen';
+import MapScreen from './screens/Map.screen';
+import DeckScreen from './screens/Deck.screen';
+import ReviewScreen from './screens/Review.screen';
+import SettingsScreen from './screens/Settings.screen.js';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
+    });
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MainNavigator />
       </View>
     );
   }
@@ -15,7 +40,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- screens/Review.screen.js
+++ screens/Review.screen.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Review Jobs',
+    headerRight: <Button onPress={() => navigation.navigate('settings')} title="Settings" />
+  })
+
   render() {
+    // console.log(this.props);
     return (
       <View>
         <Text>ReviewScreen</Text>
