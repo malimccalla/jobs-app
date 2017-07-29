850a0dfc22b9d94cfa7b081515a8d1d1526cb84f
Mali Michael <malimccalla@gmail.com>
2017-07-29 17:20:21 +0100

    Create auth actions

---
+++ actions/auth.actions.js
@@ -0,0 +1,29 @@
+import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
+import {
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
+} from './types';
+
+export const facebookLogin = () => async dispatch => {
+  let token = await AsyncStorage.getItem('fb_token');
+
+  if (token) {
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+  } else {
+    _doFacebookLogin(dispatch);
+  }
+}
+
+const _doFacebookLogin = async dispatch => {
+  let result = await Facebook.logInWithReadPermissionsAsync('1822732088057606', {
+    permissions: ['public_profile', 'email', 'user_friends']
+  });
+
+  if (result.type === 'cancel') {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL })
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+}
---
+++ actions/types.js
@@ -0,0 +1,2 @@
+export const FACEBOOK_LOGIN_SUCCESS = 'auth/facebook_login_success';
+export const FACEBOOK_LOGIN_FAIL = 'auth/facebook_login_fail';
