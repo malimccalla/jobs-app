ee7c9df8e07ca270d92c440622778dff6749a6f1
Mali Michael <malimccalla@gmail.com>
2017-07-30 18:53:48 +0100

    Render liked jobs

--- screens/Review.screen.js
+++ screens/Review.screen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, Platform } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View, Text, Platform, ScrollView, Linking } from 'react-native';
+import { Button, Card } from 'react-native-elements';
+import { MapView } from 'expo';
 import { connect } from 'react-redux';
 
 class ReviewScreen extends Component {
@@ -19,12 +20,63 @@ class ReviewScreen extends Component {
     )
   })
 
+  renderLikedJobs() {
+    return this.props.likedJobs.map((job) => {
+      const {
+        url, company, formattedRelativeTime, jobkey,
+        longitude, latitude, title, jobtitle
+      } = job;
+
+      const initialRegion = {
+        longitude,
+        latitude,
+        longitudeDelta: 0.045,
+        latitudeDelta: 0.02,
+      }
+
+      return (
+        <Card title={jobtitle} key={jobkey}>
+          <View style={{ height: 200, borderRadius: 3 }}>
+            <MapView
+              scrollEnabled={false}
+              initialRegion={initialRegion}
+              style={{ flex: 1 }}
+              provider={MapView.PROVIDER_GOOGLE}
+              cacheEnabled
+            />
+            <View style={styles.detailWrapper}>
+              <Text style={styles.italics}>{company}</Text>
+              <Text style={styles.italics}>{formattedRelativeTime}</Text>
+            </View>
+            <Button
+              title="Apply Now"
+              backgroundColor="#03A9F4"
+              onPress={() => Linking.openURL(url)}
+            />
+          </View>
+        </Card>
+      )
+    })
+  }
+
   render() {
     return (
-      <View>
-        
-      </View>
+      <ScrollView>
+        {this.renderLikedJobs()}
+      </ScrollView>
     );
+  }
+}
+
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    marginBottom: 10,
+    marginTop: 10,
+    justifyContent: 'space-around',
+  },
+  italics: {
+    fontStyle: 'italic'
   }
 }
 
