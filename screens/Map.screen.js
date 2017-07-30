9be0b52412d18c296d7377875c56725c2eb5c64b
Mali Michael <malimccalla@gmail.com>
2017-07-30 13:53:14 +0100

    Workaroiund for mapview not being loaded

--- screens/Map.screen.js
+++ screens/Map.screen.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { MapView, Permissions, Location } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    },
+  };
+
+  async componentWillMount() {
+    let response = await Permissions.askAsync(Permissions.LOCATION);
+    console.log(response);
+  }
+
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center'}}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
     return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          region={this.state.region}
+        />
       </View>
     );
   }
