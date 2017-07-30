549dbb6b351eb480be947ec48f23b11acbd02054
Mali Michael <malimccalla@gmail.com>
2017-07-30 15:00:55 +0100

    fetchJobs action creator

---
+++ actions/job.actions.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import qs from 'qs';
+import reverseGeocode from 'latlng-to-zip';
+import {
+  FETCH_JOBS
+} from './types';
+
+const JOB_URL = 'http://api.indeed.com/ads/apisearch?';
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'JavaScript'
+};
+
+export const fetchJobs = (region) => async dispatch => {
+  try {
+    let zip = await reverseGeocode(region);
+    const url = _buildJobsUrl(zip);
+    let { data } = await axios.get(url);
+    console.log(data);
+    dispatch({ type: FETCH_JOBS, payload: data });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// private
+
+const _buildJobsUrl = (zip) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_URL}${query}`;
+}
--- actions/types.js
+++ actions/types.js
@@ -1,2 +1,4 @@
 export const FACEBOOK_LOGIN_SUCCESS = 'auth/facebook_login_success';
 export const FACEBOOK_LOGIN_FAIL = 'auth/facebook_login_fail';
+
+export const FETCH_JOBS = 'jobs/fetch_jobs';
