2580f2255f09efb35cc385cf8d44fdc3f67391ec
Mali Michael <malimccalla@gmail.com>
2017-07-30 11:42:55 +0100

    Hooks up fb login to auth screen

--- screens/Auth.screen.js
+++ screens/Auth.screen.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
 
 class AuthScreen extends Component {
+  componentDidMount() {
+    this.props.facebookLogin();
+  }
+
   render() {
     return (
       <View>
@@ -15,4 +21,4 @@ class AuthScreen extends Component {
   }
 }
 
-export default AuthScreen;
+export default connect(null, actions)(AuthScreen);
